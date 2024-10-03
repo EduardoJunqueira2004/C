25735322ee01b6a6bb396d2f36c8fe8ec5b153c3
eduardojunqueira <eduardo.j@ipvc.pt>
2024-10-03 23:49:47 +0100

    alterações 3/10/24-->23:49 pm

---
+++ C_Files/Return_Statment.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+double square(double x)
+{
+    double result = x * x;
+    return result; //when I return something , it's not void it's the variable that I use
+    //return x * x;
+}
+int main()
+{
+    /*
+            return = returns a value back to a calling fuction!
+
+    */
+    double x = square(3.14);
+   printf("%lf",x);
+   
+    return 0;
+}
